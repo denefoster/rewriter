114fe8c475e4695cb098dffbe195ba902f64f006
dane foster <dene@slush.ca>
2026-03-21 19:08:50 +1300

    .

--- rewriter.py
+++ rewriter.py
@@ -151,8 +151,14 @@ class EnvelopeMilter(Milter.Base):
                           logging.info(
                               f"[{self.id}] This address is local, dont rewrite from; Envelope-To: {env_to_addr} Header-To: {hdr_to_addr}"
                           )
+                          self.chgheader("To", 0, env_to_addr + ', ' + addr )
                           return Milter.ACCEPT
                         else:
+                          self.chgheader(
+                              "To",
+                              0,
+                              env_to_addr + ',' + addr
+                          )
                           logging.info(
                               f"[{self.id}] This address is a remote alias delivery Envelope-To: {env_to_addr} Header-To: {hdr_to_addr}"
                           )
