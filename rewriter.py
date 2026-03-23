df84c3abbb06b7570e9d7d3a7d6887c6e7833bc4
dane foster <dene@slush.ca>
2026-03-23 19:01:07 +1300

    .

--- rewriter.py
+++ rewriter.py
@@ -77,7 +77,7 @@ def get_db_pool() -> ConnectionPool:
       "user": os.getenv("DB_USER", "postgres"),
       "password": os.getenv("DB_PASSWORD", "postgres"),
       "port": os.getenv("DB_PORT", "5432")
-    }, check=ConnectionPool.check_connection)
+    })
   except psycopg.OperationalError as e:
     logging.info(f"DB Error: {e}")
     raise e
@@ -86,7 +86,7 @@ def get_db_pool() -> ConnectionPool:
 
 
 def test_virtual_alias(email_addr):
-    with get_db_pool() as pool:
+    with get_db_pool(check=ConnectionPool.check_connection) as pool:
       with pool.connection() as connection:
         with connection.cursor() as cur:
           cur.execute("SELECT email from virtual where email = %s", (email_addr,))
