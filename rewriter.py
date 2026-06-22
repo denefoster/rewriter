451f9053cdd3ec6c95cd3078c892e0947a3f7e84
dane foster <dene@slush.ca>
2026-06-23 09:07:13 +1200

    .

--- rewriter.py
+++ rewriter.py
@@ -20,7 +20,7 @@ forwarding_addr = os.environ.get("FORWARDING_ADDR", "forwardingalgorithm@myaddr.
 forwarding_domain = os.environ.get("FORWARDING_DOMAIN", "myaddr.com")
 local_domains = os.environ.get("LOCAL_DOMAINS", forwarding_domain)
 rewrite_domains = os.environ.get("REWRITE_DOMAINS", "'mydomain.com': 'dmarc.mydomain.com'")
-rewrite_domain_map = ast.literal_eval(rewrite_domains)
+rewrite_domain_map = ast.literal_eval(rewrite_domains.join(","))
 
 #rewrite_domain_map = {
 #    'lists.sys.slush.ca': 'dmarc.sys.slush.ca',
