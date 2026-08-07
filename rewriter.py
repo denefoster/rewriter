ad631fa62162337164a23ac290488e5df8c1e15f
dane foster <dene@slush.ca>
2026-08-08 11:05:48 +1200

    .

--- rewriter.py
+++ rewriter.py
@@ -297,6 +297,8 @@ class EnvelopeMilter(Milter.Base):
             # no scenario match
             else:
                 logging.debug(f"{queue_id} debug: Fall through [{self.id}]")
+                logging.debug(f"{queue_id} debug: env_from is {env_from_addr} [{self.id}]")
+                logging.debug(f"{queue_id} debug: rewrite_domains are {rewrite_domain_map} [{self.id}]")
                 rewrite_domain = rewrite_domain_map[env_from_addr.split("@")[1]]
                 logging.info(f"rewrite domain is {rewrite_domain}")
                 if check_dmarc(hdr_from_addr):
