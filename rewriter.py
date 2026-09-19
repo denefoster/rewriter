1d12b2a06236e1c2174fb92f875fcfcb5f967da4
dane foster <dene@slush.ca>
2026-09-20 10:43:47 +1200

    .

--- rewriter.py
+++ rewriter.py
@@ -307,6 +307,9 @@ class EnvelopeMilter(Milter.Base):
                     rewrite_domain = forwarding_domain
                 logging.info(f"rewrite domain is {rewrite_domain}")
                 if  len(list(set(ignore_list) & set(self.mail_to))):
+                    logging.info(
+                        f"{queue_id} none: Envelope To {self.mail_to} contains an ignore list entry"
+                    )
                     return Milter.ACCEPT
                 if check_dmarc(hdr_from_addr):
                     new_hdr_from_addr = re.sub('@[^@]+$', f'=40{hdr_from_addr.rsplit('@')[-1]}@{forwarding_domain}', hdr_from_addr)
